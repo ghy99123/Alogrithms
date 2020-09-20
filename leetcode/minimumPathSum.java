6b61298f947f9bca53ef3446d6d1c0eb670981ba
Gao Hanyi <67616322+ghy99123@users.noreply.github.com>
2020-09-20 15:35:31 +0800

    64 dp

---
+++ leetcode/minimumPathSum.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int minPathSum(int[][] grid) {
+      int m=grid.length,n=grid[0].length;
+      int[][] dp=new int[m][n];
+      for(int i=0;i<m;i++)
+        for(int j=0;j<n;j++){
+          if(i==0&&j==0)
+            dp[0][0]=grid[0][0];    
+          else if(i==0)
+            dp[0][j]=dp[0][j-1]+grid[0][j];
+          else if(j==0)
+            dp[i][0]=dp[i-1][0]+grid[i][0];
+          else
+          dp[i][j]=Math.min(dp[i][j-1]+grid[i][j],dp[i-1][j]+grid[i][j]);
+        }
+          return dp[m-1][n-1];                                                    
+    }
+}
